d50d64d1a0e9b8e3965831d7b88e9fea488b1104
Manuel Sánchez <alejandromanuel94@hotmail.com>
2014-10-25 01:20:27 -0500

    Hilo que obtiene los tuits cada minuto para el servicio

--- src/org/brownielabs/android/TwitterAPIApplication.java
+++ src/org/brownielabs/android/TwitterAPIApplication.java
@@ -1,21 +1,33 @@
 package org.brownielabs.android;
 
 import android.app.Application;
+import android.content.Intent;
 import android.util.Log;
 
 public class TwitterAPIApplication extends Application {
 	
 	private static final String TAG = TwitterAPIApplication.class.getSimpleName();
+	private boolean serviceRunningFlag;
 	
+	public boolean isServiceRunningFlag() {
+		return serviceRunningFlag;
+	}
+
+	public void setServiceRunningFlag(boolean serviceRunningFlag) {
+		this.serviceRunningFlag = serviceRunningFlag;
+	}
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		Log.d(TAG, "onCreted");
+		startService(new Intent(this, UpdaterService.class));
 	}
 	
 	@Override
 	public void onTerminate() {
 		super.onTerminate();
 		Log.d(TAG, "onTerminated");
+		stopService(new Intent(this, UpdaterService.class));
 	}
 }
--- src/org/brownielabs/android/UpdaterService.java
+++ src/org/brownielabs/android/UpdaterService.java
@@ -1,5 +1,9 @@
 package org.brownielabs.android;
 
+import org.brownielabs.android.utils.ConstantsUtils;
+import org.brownielabs.android.utils.TwitterUtils;
+
+import android.app.Application;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -9,9 +13,14 @@ public class UpdaterService extends Service {
 	
 	private static final String TAG = UpdaterService.class.getSimpleName();
 
+	static final int DELAY = 60000;
+	private boolean runFlag = false;
+	
+	private Updater updater;
+	private TwitterAPIApplication application;
+	
 	@Override
 	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -19,19 +28,50 @@ public class UpdaterService extends Service {
 	public void onCreate() {
 		super.onCreate();
 		Log.d(TAG, "onCreated");
+		updater = new Updater();
+		application = (TwitterAPIApplication) getApplication();
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		Log.d(TAG, "onDestroyed");
+		runFlag = false;
+		application.setServiceRunningFlag(false);
+		updater.interrupt();
+		updater = null;
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		super.onStartCommand(intent, flags, startId);
 		Log.d(TAG, "onStarted");
+		runFlag = true;
+		application.setServiceRunningFlag(true);
+		updater.start();
 		return START_STICKY;
+	}
+	
+	private class Updater extends Thread{
+		
+		public Updater(){
+			super("UpdaterService-UpdaterThread");
+		}
+		
+		@Override
+		public void run() {
+			UpdaterService updaterService = UpdaterService.this;
+			while(updaterService.runFlag){
+				Log.d(TAG, "UpdaterThread running");
+				try{
+					TwitterUtils.getTimelineForSearchTerm(ConstantsUtils.SEARCH_TERM);
+					Thread.sleep(DELAY);
+				}catch(InterruptedException e){
+					updaterService.runFlag = false;
+					application.setServiceRunningFlag(false);
+				}
+			}
+		}
 	}
 	
 }
